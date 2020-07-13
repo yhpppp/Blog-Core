f0b35e003528c2f4c026df1fa06088f93e56abc6
yhpp <yhpp1995@gmail.com>
2020-07-13 17:33:25 +0800

    feat: 添加 swagger

--- Blog.Core/Startup.cs
+++ Blog.Core/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Blog.Core
 {
@@ -25,6 +26,17 @@ namespace Blog.Core
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new Info
+                {
+                    Version = "v0.1.0",
+                    Title = "Blog.Core API",
+                    Description = "框架说明文档",
+                    TermsOfService = "None",
+                    Contact = new Contact { Name = "Blog.Core", Email = "Blog.Core@xxx.com", Url = "/" }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +46,14 @@ namespace Blog.Core
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(config =>
+            {
+                config.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiHelp V1");
+                config.RoutePrefix = ""; // 根目录直接访问swagger
+
+            });
 
             app.UseMvc();
         }
