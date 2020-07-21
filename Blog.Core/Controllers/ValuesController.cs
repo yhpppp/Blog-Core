acd5b9e983a96be08355cb697a80458c6ca6673b
yhpp <yhpp1995@gmail.com>
2020-07-21 17:39:07 +0800

    feat: swagger 添加注释

--- Blog.Core/Controllers/ValuesController.cs
+++ Blog.Core/Controllers/ValuesController.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.Core.Model;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Core.Controllers
-{
+{   
+    /// <summary>
+    /// 测试控制器
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        /// <summary>
+        /// 获取测试数据列表
+        /// </summary>
+        /// <returns></returns>
         // GET api/values
         [HttpGet]
+        //[Authorize(Roles = "Admin")]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
@@ -25,11 +35,18 @@ namespace Blog.Core.Controllers
         }
 
         // POST api/values
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+        /// <summary>
+        /// test post
+        /// </summary>
+        /// <param name="love">model实体类参数</param>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post(Love love)
         {
         }
-
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
@@ -38,6 +55,8 @@ namespace Blog.Core.Controllers
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
+        // 文档隐藏接口
+        [ApiExplorerSettings(IgnoreApi =true)]
         public void Delete(int id)
         {
         }
--- Blog.Core/Startup.cs
+++ Blog.Core/Startup.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Blog.Core
 {
@@ -25,7 +30,13 @@ namespace Blog.Core
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options =>
+            {
+                // Output xml
+                //options.ReturnHttpNotAcceptable = true;
+                //options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
             services.AddSwaggerGen(config =>
             {
                 config.SwaggerDoc("v1", new Info
@@ -36,7 +47,22 @@ namespace Blog.Core
                     TermsOfService = "None",
                     Contact = new Contact { Name = "Blog.Core", Email = "Blog.Core@xxx.com", Url = "/" }
                 });
+
+                // 配置自定义注释
+                var basePath = AppContext.BaseDirectory;
+                var xmlPath = Path.Combine(basePath, "Blog.core.xml");
+                config.IncludeXmlComments(xmlPath, true);
+                // 引用Model注释配置
+                var xmlModelPath = Path.Combine(basePath, "Blog.Core.Model.xml");
+                config.IncludeXmlComments(xmlModelPath);
+
+             
             });
+
+   
+
+          
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,7 +80,6 @@ namespace Blog.Core
                 config.RoutePrefix = ""; // 根目录直接访问swagger
 
             });
-
             app.UseMvc();
         }
     }
